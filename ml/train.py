3596d5b977667c284be8888f7fc861df9b407154
Stephanie Couto <stephanie.couto@cdw.com>
2025-09-16 15:55:02 -0400

    ensure ws MI can write to storage

--- ml/train.py
+++ ml/train.py
@@ -1,4 +1,4 @@
-import os, sys, joblib
+import os, sys, joblib   # <-- add sys
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -11,7 +11,6 @@ iris = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(
     iris.data, iris.target, test_size=0.2, random_state=42
 )
-
 clf = RandomForestClassifier(n_estimators=100, random_state=42).fit(X_train, y_train)
 acc = clf.score(X_test, y_test)
 print(f"Accuracy={acc:.3f}")
